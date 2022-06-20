7ccc4c4811050711ac46995b69d2c1c779854c7d
Matteo Landi <matteo@matteolandi.net>
2022-06-20 10:22:19 +0530

    Add `ClassSerializerInterceptor`

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
